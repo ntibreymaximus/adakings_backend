b9caee5339c40a411c640e67326d5ae272913c25
ntibreymaximus <ntibreymaximus@gmail.com>
2025-05-23 15:09:04 +0000

    fix(users): Update model references and role checks
    - Change User model reference to CustomUser
    - Fix role check method calls
    - Update all view classes to use correct model
    - Maintain proper access control checks

--- apps/users/views.py
+++ apps/users/views.py
@@ -14,7 +14,7 @@ from django.db.models import Count, Sum
 from django.utils import timezone
 from datetime import timedelta
 
-from .models import User
+from .models import CustomUser
 from .forms import (
     CustomUserCreationForm, CustomUserChangeForm,
     CustomAuthenticationForm, CustomPasswordResetForm,
@@ -25,7 +25,7 @@ from apps.orders.models import Order
 def admin_required(function):
     """Decorator for views that checks that the user is admin."""
     actual_decorator = user_passes_test(
-        lambda u: u.is_active and u.is_admin,
+        lambda u: u.is_active and u.is_admin(),
         login_url='users:access_denied'
     )
     if function:
@@ -35,7 +35,7 @@ def admin_required(function):
 def frontdesk_required(function):
     """Decorator for views that checks that the user is frontdesk staff."""
     actual_decorator = user_passes_test(
-        lambda u: u.is_active and (u.is_frontdesk or u.is_admin),
+        lambda u: u.is_active and (u.is_frontdesk() or u.is_admin()),
         login_url='users:access_denied'
     )
     if function:
@@ -53,13 +53,13 @@ class CustomLogoutView(LogoutView):
 @login_required
 def dashboard_redirect(request):
     """Redirect users to their role-specific dashboard."""
-    if request.user.is_admin:
+    if request.user.is_admin():
         return redirect('users:admin_dashboard')
-    elif request.user.is_frontdesk:
+    elif request.user.is_frontdesk():
         return redirect('users:frontdesk_dashboard')
-    elif request.user.is_kitchen:
+    elif request.user.is_kitchen():
         return redirect('users:kitchen_dashboard')
-    elif request.user.is_delivery:
+    elif request.user.is_delivery():
         return redirect('users:delivery_dashboard')
     return redirect('users:access_denied')
 
@@ -67,7 +67,7 @@ class AdminDashboardView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     template_name = 'users/dashboards/admin_dashboard.html'
     
     def test_func(self):
-        return self.request.user.is_admin
+        return self.request.user.is_admin()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -120,7 +120,7 @@ class KitchenDashboardView(LoginRequiredMixin, UserPassesTestMixin, TemplateView
     template_name = 'users/dashboards/kitchen_dashboard.html'
     
     def test_func(self):
-        return self.request.user.is_kitchen
+        return self.request.user.is_kitchen()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -131,7 +131,7 @@ class DeliveryDashboardView(LoginRequiredMixin, UserPassesTestMixin, TemplateVie
     template_name = 'users/dashboards/delivery_dashboard.html'
     
     def test_func(self):
-        return self.request.user.is_delivery
+        return self.request.user.is_delivery()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -142,36 +142,36 @@ class AccessDeniedView(TemplateView):
     template_name = 'users/access_denied.html'
 
 class StaffRegistrationView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, CreateView):
-    model = User
+    model = CustomUser
     form_class = CustomUserCreationForm
     template_name = 'users/staff_form.html'
     success_url = reverse_lazy('users:staff_list')
     success_message = "Staff member was created successfully"
     
     def test_func(self):
-        return self.request.user.is_admin
+        return self.request.user.is_admin()
 
 class StaffListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
-    model = User
+    model = CustomUser
     template_name = 'users/staff_list.html'
     context_object_name = 'staff_members'
     
     def test_func(self):
-        return self.request.user.is_admin
+        return self.request.user.is_admin()
     
     def get_queryset(self):
-        return User.objects.exclude(pk=self.request.user.pk)
+        return CustomUser.objects.exclude(pk=self.request.user.pk)
 
 class StaffDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
-    model = User
+    model = CustomUser
     template_name = 'users/staff_detail.html'
     context_object_name = 'staff_member'
     
     def test_func(self):
-        return self.request.user.is_admin
+        return self.request.user.is_admin()
 
 class ProfileUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
-    model = User
+    model = CustomUser
     form_class = CustomUserChangeForm
     template_name = 'users/profile_update.html'
     success_url = reverse_lazy('users:profile')
