100b1fd936cade82dc2e539736a73f24fdd03732
ntibreymaximus <ntibreymaximus@gmail.com>
2025-07-07 23:41:15 +0000

    fix: Update ASGI settings module path to match new settings location

--- adakings_backend/asgi.py
+++ adakings_backend/asgi.py
@@ -11,6 +11,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adakings_backend.settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adakings_backend.settings')
 
 application = get_asgi_application()
